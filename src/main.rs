b29c9e05e6dc34a8d3348023aeeabca1d1e293d3
Piyush Raj Sharma <rpiyush1357@gmail.com>
2025-11-09 02:07:38 +0530

    - Implemented a basic command-line number guessing game - Used rand::Rng for random number generation - Handled user input via std::io - Compared user guess with the secret number using std::cmp::Ordering - Demonstrated variable shadowing and input parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let secert_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is {secert_number}");
+
+    loop {
+        println!("Please input your guess");
+        let mut guess = String::new(); // In rust the variables are immutable by default, 
+        // so we use mut keyword to make it mutable
+
+        io::stdin().read_line(&mut guess).expect("Failed to read"); // read_line() takes a mutable reference to the string
+        let guess: u32 = guess.trim().parse().expect("Please type a number!"); // trim() removes whitespace and parse() converts string to number
+        // Rust allows shadowing, so we can reuse the variable name guess rather than creating a new variable i.e. let guess2
+        println!("You guessed {guess}");
+
+        match guess.cmp(&secert_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win!"),
+        }
+    }
 }
